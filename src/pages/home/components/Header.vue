94ff25a3d6c872ef136507ec676d2f68bc86ab49
jiaen188 <244189981@qq.com>
2018-05-30 21:30:57 +0800

    header部分和字体图标

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -19,16 +27,20 @@ export default {
     line-height: 0.86rem
     height: .86rem
     background: #00bcd4
-    color #fff
+    color: #fff
     .header-left
       width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
-      height 0.64rem
-      line-height 0.64rem
-      margin-top 0.12rem
-      margin-left 0.2rem
+      height: 0.64rem
+      line-height: 0.64rem
+      margin-top: 0.12rem
+      margin-left: 0.2rem
+      padding-left: 0.2rem
       background: #fff
       border-radius: .1rem
       color: #ccc
@@ -36,4 +48,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      .arrow-icon
+        margin-left: -0.04rem
+        font-size: .24rem
 </style>
