7b4f7593630292e738326d93ce2c1082fb016bc3
jiaen188 <244189981@qq.com>
2018-06-11 16:22:07 +0800

    推荐部分和周末去哪儿

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -21,7 +21,32 @@ export default {
   name: 'HomeRecommend',
   data () {
     return {
-      recommendList: []
+      recommendList: [{
+        'id': '0001',
+        'imgUrl': 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+        'title': '故宫',
+        'desc': '东方宫殿建筑代表，世界宫殿建筑典范'
+      }, {
+        'id': '0002',
+        'imgUrl': 'http://img1.qunarzz.com/sight/p0/1511/d2/d2aec2dfc5aa771290.water.jpg_140x140_abb362a7.jpg',
+        'title': '南山滑雪场',
+        'desc': '北京专业级滑雪圣地'
+      }, {
+        'id': '0003',
+        'imgUrl': 'http://img1.qunarzz.com/sight/p0/1501/f4/f467729126949c3a.water.jpg_140x140_ef235b1c.jpg',
+        'title': '天安门广场',
+        'desc': '我爱北京天安门，天安门上太阳升'
+      }, {
+        'id': '0004',
+        'imgUrl': 'http://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_140x140_1c863e5c.jpg',
+        'title': '水立方',
+        'desc': '中国的荣耀，阳光下的晶莹水滴'
+      }, {
+        'id': '0005',
+        'imgUrl': 'http://img1.qunarzz.com/sight/p0/201308/23/b283071686e64dfec8d65eac.jpg_140x140_8c5a7c49.jpg',
+        'title': '温都水城养生馆',
+        'desc': '各种亚热带植物掩映其间仿佛置身热带雨林'
+      }]
     }
   }
 }
@@ -30,10 +55,10 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/mixins.styl'
   .title
-    margin-top 2rem
+    margin-top .2rem
     line-height 0.8rem
     background-color #eee
-    text-indent 2em
+    text-indent .2rem
   .item
     overflow hidden
     display flex
@@ -55,7 +80,7 @@ export default {
         color #ccc
         ellipsis()
       .item-button
-        line-height 0.44re
+        line-height 0.44rem
         margin-top 0.16rem
         background-color #ff9300
         padding 0 .2rem
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="title">
+      周末去哪儿
+    </div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        'id': '0001',
+        'imgUrl': 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+        'title': '故宫',
+        'desc': '东方宫殿建筑代表，世界宫殿建筑典范'
+      }, {
+        'id': '0002',
+        'imgUrl': 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+        'title': '南山滑雪场',
+        'desc': '北京专业级滑雪圣地'
+      }, {
+        'id': '0003',
+        'imgUrl': 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+        'title': '天安门广场',
+        'desc': '我爱北京天安门，天安门上太阳升'
+      }, {
+        'id': '0004',
+        'imgUrl': 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+        'title': '水立方',
+        'desc': '中国的荣耀，阳光下的晶莹水滴'
+      }, {
+        'id': '0005',
+        'imgUrl': 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+        'title': '温都水城养生馆',
+        'desc': '各种亚热带植物掩映其间仿佛置身热带雨林'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top .2rem
+    line-height 0.8rem
+    background-color #eee
+    text-indent .2rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+  .item-info
+    padding 0.1rem
+    .item-title
+      line-height 0.54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height 0.4rem
+      color #ccc
+      ellipsis()
+</style>
