a133f54740d99c9665da30307c55b45aa02e595a
jiaen188 <244189981@qq.com>
2018-06-11 21:52:34 +0800

    city的列表部分

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">杭州</div>
+        </div>
+      </div>
+    </div>
+     <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">杭州</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">杭州</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">杭州</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">杭州</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">杭州</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height 0.44rem
+      background #eee
+      padding-left 0.2rem
+      color #666
+      font-size .26rem
+    .button-list
+      overflow hidden
+      padding 0.1rem 0.6rem 0.1rem 0.1rem
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          text-align center
+          padding 0.1rem 0
+          margin 0.1rem
+          border 0.02rem solid #ccc
+          border-radius 0.06rem
+    .item-list
+      .item
+        line-height 0.76rem
+        padding-left 0.2rem
+</style>
