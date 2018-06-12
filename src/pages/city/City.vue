4d05e18502a3fdfb816b4bd614e3d290c3453945
jiaen188 <244189981@qq.com>
2018-06-12 22:30:12 +0800

    搜索页面实现

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search
+      :cities="cities"
+    ></city-search>
     <city-list
       :cities="cities"
       :hot="hotCities"
