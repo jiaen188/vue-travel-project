bf6770c31a6712fd56d7047a0d666ab4b58f67cb
jiaen188 <244189981@qq.com>
2018-06-14 14:32:45 +0800

    banner部分完成

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -4,14 +4,20 @@
       热销推荐
     </div>
     <ul>
-      <li class="item border-bottom" v-for="item of list" :key="item.id">
+      <router-link
+        tag="li"
+        class="item border-bottom"
+        v-for="item of list"
+        :key="item.id"
+        :to="'/detail/' + item.id"
+      >
         <img :src="item.imgUrl" class="item-img">
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
